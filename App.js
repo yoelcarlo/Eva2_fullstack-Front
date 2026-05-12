d549b84de6e05436f51a98b6fca230dab1b45c21
yoelcarlo <car.vasquezp@duocuc.cl>
2026-05-11 20:32:18 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SupervivientesTerminal from './components/SupervivientesTerminal';
+import './styles/terminal.css';
+
+function App() {
+  return (
+    <div>
+      <SupervivientesTerminal />
+    </div>
+  );
+}
+
+export default App;
